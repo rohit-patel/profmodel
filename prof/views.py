bac0d34cb5e87dff4794ad27f6c9343a18a5dba0
Rohit Patel <rohit.patel.ecn+c9@gmail.com>
2017-01-19 21:20:20 +0000

    Fixed the logout so that now the page redirects to the right destination. Also, fixed the permissions decorator so that now only the people who are file owners can access a particular run. Now moving to work on the runhome and actual calcs.

--- prof/views.py
+++ prof/views.py
@@ -7,11 +7,12 @@ from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
 from guardian.shortcuts import get_objects_for_user, assign_perm
 from guardian.decorators import permission_required_or_403
+from django.contrib.auth.models import User
 
 def index(request):
     return render(request, 'prof/plain.html')
 
-@login_required    
+@login_required    #This decorator makes sure that a user is logged in
 def userhome(request):
     runpk=1
     company= RunSpace.objects.get(pk=runpk).CompanyName
@@ -19,20 +20,22 @@ def userhome(request):
     return render(request, 'prof/userhome.html', {'runpk': runpk, 'company' : company, 'run': run})
     
 @login_required
-#@permission_required_or_403('prof.file_owner',(request.user,RunSpace.objects.get(pk=runpk)))  #This does not work, but we need a decorator somehow
+@permission_required_or_403('prof.file_owner',(RunSpace, 'pk', 'runpk' ))  #This decorator makes sure that the user that is logged in has 
 def runhome(request,runpk):
     runobject=RunSpace.objects.get(pk=runpk)
     company = runobject.CompanyName
     local_runno=runobject.RunNo
-    #if request.method=='POST':
-    #    form=FileSpaceForm(request.POST, request.FILES)
-    #    if form.is_valid():
-    #        newfile = FileSpace(CompanyName=request.POST['CompanyName'], RunNo = request.POST['RunNo'], RunName = request.POST['RunName'], FileType = request.POST['FileType'], TheActualFile=request.FILES['TheActualFile'])
-    #        newfile.save()
-    #        assign_perm('prof.file_owner',request.user,newfile)  #This is causing some sort of an error, understand how permission assigning in django-guardian works.
-    #        return HttpResponseRedirect(reverse('prof:runhome',kwargs={'runid' : runpk}, current_app='prof'))
-    #else:
-    form=FileSpaceForm() #tab is when reinserting the if statements above
+    
+    if request.method=='POST':
+        form=FileSpaceForm(request.POST, request.FILES)
+        if form.is_valid():
+            newfile = FileSpace(UniqueRunID = RunSpace.objects.get(pk=runpk), FileType = request.POST['FileType'], FileName = request.POST['FileName'], TheActualFile=request.FILES['TheActualFile'])
+            newfile.save()
+            #assign_perm('prof.file_owner',request.user,newfile)  #This permission assignment is no longer needed because of the model shift, files do not have permission attributes anymore.
+            return HttpResponseRedirect(reverse('prof:runhome', kwargs={'runpk' : runpk}, current_app='prof'))
+            #return render(request, 'prof/plain.html',{'runpk': runpk})  #Just some test code to see behavior
+    else:
+        form=FileSpaceForm()
         
     relevant_files=FileSpace.objects.filter(UniqueRunID=runobject)  #get_objects_for_user(request.user,'prof.file_owner').filter(CompanyName=company, RunNo = run)
     pnl_files=relevant_files.filter(FileType='P')
